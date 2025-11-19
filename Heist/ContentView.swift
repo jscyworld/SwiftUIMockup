a2af279427086c2932ba33b24985cff4182a4a26
Sirius Kim <sirius@haii.io>
2025-11-19 09:25:39 +0900

    Initial Commit

---
+++ Heist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Heist
+//
+//  Created by Sirius_Haii on 11/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Heist/HeistApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeistApp.swift
+//  Heist
+//
+//  Created by Sirius_Haii on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HeistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
